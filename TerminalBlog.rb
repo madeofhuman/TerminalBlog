258856fca5c0a23af4827637bf21009d5463c1cb
madeofhuman <josephodina@gmail.com>
2017-01-23 10:46:14 +0100

    Post upload

--- TerminalBlog.rb
+++ TerminalBlog.rb
@@ -6,7 +6,10 @@ require 'google/api_client/client_secrets'
 require 'json'
 require 'launchy'
 
-blogId = 0 			# the blog id
+# get the blog id
+print "Enter your blogId: "
+
+blogId = STDIN.gets.to_i 			# the blog id
 isDraft = true 			# option to upload as draft. --publish argument overrides it
 postfile = '' 			# path to blogpost file
 title = 'Default Title' 			# default post title. -t argument overrides it
@@ -43,8 +46,7 @@ end
 
 # if there is no userkey, authenticate with Google and save the key
 if File.exist?("userkey") == false
-	# Launchy.open("http://google.com")
-	client_secrets = Google::APIClient::ClientSecrets.load
+	client_secrets = Google::APIClient::ClientSecrets.load("client_secrets.json")
 	auth_client = client_secrets.to_authorization
 	auth_client.update!(:scope => 'https://www.googleapis.com/auth/blogger',
 		:redirect_uri => 'urn:ietf:wg:oauth:2.0:oob')
@@ -52,18 +54,55 @@ if File.exist?("userkey") == false
 	auth_uri = auth_client.authorization_uri.to_s
 	Launchy.open(auth_uri)
 
-	puts 'Paste the code from the auth response page'
-	auth_client.code = gets
+	print 'Paste the code from the auth response page: '
+	credential = gets
+	auth_client.code = credential
 	auth_client.fetch_access_token!
 
-	blogger = Google::Apis::BloggerV3::BloggerService.new
-	blogger.authorization = auth_client
+	puts auth_client.fetch_access_token.to_s
 
 	# store key for future reference
 	userkey = File.new("userkey","w")
-	userkey.puts(auth_client.code)
+	userkey.puts(auth_client.fetch_access_token.to_s)
 else
-	userkey = File.read("userkey")
-	auth_client_code = userkey
-	auth_client.fetch_access_token!
+	client_secrets = Google::APIClient::ClientSecrets.load(client_secrets)
+	auth_client = client_secrets.to_authorization
+	credential = File.read("userkey")
+	auth_client.code = credential
+	#auth_client.fetch_access_token!
 end
+
+# initialize blogger service
+blogger = Google::Apis::BloggerV3::BloggerService.new
+blogger.authorization = auth_client
+
+# open file
+begin
+	f = File.read(postfile)
+rescue Exception => e
+	abort("Error opening file. Aborting...")
+end
+
+# buid a label list
+labels_list = labels.split(',')
+
+# build body object
+body = {
+	"content": f,
+	"title": title,
+	"labels": labels_list
+}
+
+# post to Blogger service
+begin
+	post = blogger.Posts.insert(blogId=blogId,body=body,isDraft=isDraft).execute
+rescue Exception => e
+	abort("Google didn't like our post :(")
+end
+
+puts "Title: %s" % post['title']
+puts "isDraft: %s" % isDraft
+if isDraft == false
+	puts "URL: %s" % post['url']
+end
+puts "Labels: %s" % post['labels']
